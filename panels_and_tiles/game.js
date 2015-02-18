4c9a7c3d5827a824e55bd4bb3e6f9b0ce56b376d
tylerbdev <tyler@tylerbarton.me>
2015-02-18 13:38:49 -0800

    updated drag and drop

--- panels_and_tiles/game.js
+++ panels_and_tiles/game.js
@@ -1,11 +1,56 @@
  $(function() {
-$( ".drag-tile" ).draggable();
-$( "#drop-area" ).droppable({
-drop: function( event, ui ) {
-$( this )
-.addClass( "ui-state-highlight" )
-.find( "p" )
-.html( "Dropped!" );
-}
+  $( ".drag-tile" ).draggable({
+    revert: "invalid",
+    snap: ".drop-area",
+    snapMode: "inner",
+    snapTolerance:"1px",
+  });
+  var last_dropped_id = '';
+  // var last_pos = '';
+  $( ".drop-area" ).droppable({
+       
+       tolerance: "touch",
+    drop: function( event, ui ) {
+
+      var valMy = "top";
+      var valAt = "top";
+      if(last_dropped_id != undefined){
+        var last_pos = $(last_dropped_id).position();
+        
+        
+        if(last_pos != undefined){
+         
+           
+        valAt+= "+" + last_pos.top;
+        } 
+        
+       
+
+      }
+      var $this = $(this);
+    
+      ui.draggable.position({
+        my: valMy,
+        at: valAt,
+        of: $this,
+        using: function(pos) {
+          $(this).animate(pos, 400, "linear");
+
+       },
+
+      });
+      
+     
+        last_dropped_id = '#' + ui.draggable.attr('id');
+      
+      console.log(last_dropped_id);
+    }
+       
+   
+
+  });
+ 
+
 });
-});
+
+
