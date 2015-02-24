141ba9f0250ef132bfbe489d4b063c544a288c39
tylerbdev <tyler@tylerbarton.me>
2015-02-23 20:47:35 -0800

    drag and drop and delete
    working now, switched from one droppable area to multiple

--- panels_and_tiles/game.js
+++ panels_and_tiles/game.js
@@ -1,5 +1,7 @@
  $(function() { 
 
+  //choice id : area id
+  var where_is_what = [];
 
   $('[id*=del]').click(function(e) {
     
@@ -11,34 +13,9 @@
     $(deleted_tile).draggable( "enable" );
     $(deleted_tile + '-del').attr('style', 'display:none');
     $(deleted_tile).attr('data-status', 'unselected');
-
-    //clean up remaining tiles
-    //get all tiles with offets of [overthere], for tilesthere[count],
-    //tilesthere[count].offset-top = 0 + count*[height_of_tile];
-
-    var selected_tiles = $("[id^=choice-]");
-       //remove deleted buttons from selected_tiles array
-    for(var count = 0; count<selected_tiles.length; count++){
-      if ( selected_tiles[count] != undefined && selected_tiles[count]['id'].substr((selected_tiles[count]['id'].length)-4, selected_tiles[count]['id'].length) == '-del'){
-        selected_tiles.splice(count, 1);
-      } 
+    //reenable droppable area
+    $('#' + where_is_what[deleted_tile]).droppable({disabled:false});
       
-      if(selected_tiles[count] != undefined && $('#' + selected_tiles[count]['id']).attr('data-status') == 'unselected'){
-        selected_tiles.splice(count,1);
-      }
-    }
-    //reposition selected tiles
-    for(var count = 0; count<selected_tiles.length; count++){
-      var current_id = '#' + selected_tiles[count]['id'];
-      var current_offset = $(current_id).offset();
-      left_offset = current_offset['left'];
-      top_offset = current_offset['top'];
-     
-      $(current_id).offset({top: (top_offset-93), left: left_offset});
-
-    } 
-    
-    
   });
 
 
@@ -59,27 +36,50 @@
   });
   var last_dropped_id = '';
  
-  $( ".drop-area" ).droppable({
+  $( "#area-1" ).droppable({
        
-       tolerance: "touch",
+       tolerance: "pointer",
     drop: function( event, ui ) {
       $('#' + ui.draggable.attr('id') + '-del').attr('style', 'display:block');
       $('#' + ui.helper.attr("id")).attr('data-status', 'selected');
       var valMy = "top";
       var valAt = "top";
-      if(last_dropped_id != undefined){
-        var last_pos = $(last_dropped_id).position();
-        
-        
-        if(last_pos != undefined){
-         
-           
-        valAt+= "+" + last_pos.top;
-        } 
-        
-       
+    
+      var $this = $(this);
+    
+      ui.draggable.position({
+        my: valMy,
+        at: valAt,
+        of: $this,
+        using: function(pos) {
+          $(this).animate(pos, 200, "linear");
 
-      }
+       },
+
+
+
+      });
+     
+        
+        last_dropped_id = '#' + ui.draggable.attr('id');
+         $( last_dropped_id).draggable( "disable" );
+      $this.droppable({
+        disabled: true }
+        );
+     
+      where_is_what[last_dropped_id] = $this[0].id;
+   
+    }
+  });
+  $( "#area-2" ).droppable({
+       
+       tolerance: "pointer",
+    drop: function( event, ui ) {
+      $('#' + ui.draggable.attr('id') + '-del').attr('style', 'display:block');
+      $('#' + ui.helper.attr("id")).attr('data-status', 'selected');
+      var valMy = "top";
+      var valAt = "top";
+    
       var $this = $(this);
     
       ui.draggable.position({
@@ -96,14 +96,112 @@
      
         last_dropped_id = '#' + ui.draggable.attr('id');
          $( last_dropped_id).draggable( "disable" );
-      
-      console.log(last_dropped_id);
+      $this.droppable({
+        disabled: true }
+        );
+         where_is_what[last_dropped_id] = $this[0].id;
     }
+    
+    });
+   $( "#area-3" ).droppable({
        
-   
+       tolerance: "pointer",
+    drop: function( event, ui ) {
+      $('#' + ui.draggable.attr('id') + '-del').attr('style', 'display:block');
+      $('#' + ui.helper.attr("id")).attr('data-status', 'selected');
+      var valMy = "top";
+      var valAt = "top";
+    
+      var $this = $(this);
+    
+      ui.draggable.position({
+        my: valMy,
+        at: valAt,
+        of: $this,
+        using: function(pos) {
+          $(this).animate(pos, 200, "linear");
 
-  });
- 
+       },
+
+      });
+     
+     
+        last_dropped_id = '#' + ui.draggable.attr('id');
+         $( last_dropped_id).draggable( "disable" );
+      $this.droppable({
+        disabled: true }
+        );
+         where_is_what[last_dropped_id] = $this[0].id;
+    }
+    
+    });
+
+ $( "#area-4" ).droppable({
+       
+       tolerance: "pointer",
+    drop: function( event, ui ) {
+      $('#' + ui.draggable.attr('id') + '-del').attr('style', 'display:block');
+      $('#' + ui.helper.attr("id")).attr('data-status', 'selected');
+      var valMy = "top";
+      var valAt = "top";
+    
+      var $this = $(this);
+    
+      ui.draggable.position({
+        my: valMy,
+        at: valAt,
+        of: $this,
+        using: function(pos) {
+          $(this).animate(pos, 200, "linear");
+
+       },
+
+      });
+     
+     
+        last_dropped_id = '#' + ui.draggable.attr('id');
+         $( last_dropped_id).draggable( "disable" );
+      $this.droppable({
+        disabled: true }
+        );
+         where_is_what[last_dropped_id] = $this[0].id;
+    }
+    
+    });
+
+ $( "#area-5" ).droppable({
+       
+       tolerance: "pointer",
+    drop: function( event, ui ) {
+      $('#' + ui.draggable.attr('id') + '-del').attr('style', 'display:block');
+      $('#' + ui.helper.attr("id")).attr('data-status', 'selected');
+      var valMy = "top";
+      var valAt = "top";
+    
+      var $this = $(this);
+    
+      ui.draggable.position({
+        my: valMy,
+        at: valAt,
+        of: $this,
+        using: function(pos) {
+          $(this).animate(pos, 200, "linear");
+
+       },
+
+      });
+     
+     
+        last_dropped_id = '#' + ui.draggable.attr('id');
+         $( last_dropped_id).draggable( "disable" );
+      $this.droppable({
+        disabled: true }
+        );
+         where_is_what[last_dropped_id] = $this[0].id;
+    }
+    
+    });
+
 
 });
 
