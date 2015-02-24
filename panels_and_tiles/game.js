fba8684e126c7997f30c43017e1d75da8e8c4f78
tylerbdev <tyler@tylerbarton.me>
2015-02-23 18:27:39 -0800

    more work on drag and drop
    trying something else, this commit is just a backup essentially

--- panels_and_tiles/game.js
+++ panels_and_tiles/game.js
@@ -2,14 +2,41 @@
 
 
   $('[id*=del]').click(function(e) {
+    
+    //move tile back over and hide delete button
     deleted_tile = '#' + e.target['id'].substr(0, (e.target['id'].length)-4);
     reverted_left = $(deleted_tile).attr('data-orig-left');
     reverted_top = $(deleted_tile).attr('data-orig-top');
     $( deleted_tile ).offset({ top: reverted_top, left: reverted_left });
     $(deleted_tile).draggable( "enable" );
     $(deleted_tile + '-del').attr('style', 'display:none');
+    $(deleted_tile).attr('data-status', 'unselected');
 
+    //clean up remaining tiles
+    //get all tiles with offets of [overthere], for tilesthere[count],
+    //tilesthere[count].offset-top = 0 + count*[height_of_tile];
+
+    var selected_tiles = $("[id^=choice-]");
+       //remove deleted buttons from selected_tiles array
+    for(var count = 0; count<selected_tiles.length; count++){
+      if ( selected_tiles[count] != undefined && selected_tiles[count]['id'].substr((selected_tiles[count]['id'].length)-4, selected_tiles[count]['id'].length) == '-del'){
+        selected_tiles.splice(count, 1);
+      } 
       
+      if(selected_tiles[count] != undefined && $('#' + selected_tiles[count]['id']).attr('data-status') == 'unselected'){
+        selected_tiles.splice(count,1);
+      }
+    }
+    //reposition selected tiles
+    for(var count = 0; count<selected_tiles.length; count++){
+      var current_id = '#' + selected_tiles[count]['id'];
+      var current_offset = $(current_id).offset();
+      left_offset = current_offset['left'];
+      top_offset = current_offset['top'];
+     
+      $(current_id).offset({top: (top_offset-93), left: left_offset});
+
+    } 
     
     
   });
@@ -31,12 +58,13 @@
     }
   });
   var last_dropped_id = '';
-  // var last_pos = '';
+ 
   $( ".drop-area" ).droppable({
        
        tolerance: "touch",
     drop: function( event, ui ) {
       $('#' + ui.draggable.attr('id') + '-del').attr('style', 'display:block');
+      $('#' + ui.helper.attr("id")).attr('data-status', 'selected');
       var valMy = "top";
       var valAt = "top";
       if(last_dropped_id != undefined){
